7ccef0d64b5e7ea2dfe2572601c88def93036e09
OJThings <netfilxkenya13@gmail.com>
2023-04-22 13:52:18 +0800

    last

--- backend/app.js
+++ backend/app.js
@@ -24,10 +24,10 @@ const server = () => {
 }
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+    app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
+      res.sendFile(path.join(__dirname , '..', 'frontend', 'build', 'index.html'))
     });
     
   }
