61c253d7c966647f58511171b5a2817651664b6a
OJThings <netfilxkenya13@gmail.com>
2023-04-22 15:43:42 +0800

    s

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,40 @@
-const express = require('express')
-const cors = require('cors');
-const { db } = require('./db/db');
-const {readdirSync} = require('fs')
-const path = require('path');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
+const path = require("path");
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://expnsetrck.onrender.com"],
+  })
+);
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
+  db();
+  app.listen(PORT, () => {
+    console.log("listening to port:", PORT);
+  });
+};
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+  });
 }
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname , '..', 'frontend', 'build', 'index.html'))
-    });
-    
-  }
-
-server()
+server();
